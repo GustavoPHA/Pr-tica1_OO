96ddcedc5423998ac012e35bb89f19b62b0dba91
Gustavo Philip <111594616+GustavoPHA@users.noreply.github.com>
2022-09-01 21:54:55 -0300

    Update Aluno.java

--- Aula2/src/br.com.aula2.exercicio/Aluno.java
+++ Aula2/src/br.com.aula2.exercicio/Aluno.java
@@ -5,7 +5,7 @@ public class Aluno {
     private String curso;
     private int periodo;
 
-    public Aluno(String nome, int matricula, float notaAV1, float notaAV2, float notaAE, String curso, int periodo){
+    public Aluno(String nome, int matricula, float notaAV1, float notaAV2, float notaAE, String curso, int periodo) {
         super();
         this.nome = nome;
         this.matricula = matricula;
@@ -15,5 +15,31 @@ public class Aluno {
         this.curso = curso;
         this.periodo = periodo;
 
+    }
+
+    public void alteranotaAV1(float nota) {
+        this.notaAV1 = nota;
+    }
+
+    public void alteranotaAv2(float nota) {
+        this.notaAV2 = nota;
+    }
+
+    public void alteranotaAE(float nota) {
+        this.notaAE = nota;
+    }
+
+    public String avaliarAluno() {
+        if ((this.notaAV1 + this.notaAV2) >= 60)
+            return "Aprovado";
+        else
+            return "Recuperação";
+    }
+
+    public String avaliarRecuperacao() {
+        if ((this.notaAV1 + this.notaAV2 + this.notaAE) / 2 >= 60)
+            return "Aprovado";
+        else
+            return "Reprovado";
     }
 }
